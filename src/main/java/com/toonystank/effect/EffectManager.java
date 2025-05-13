c2af5d7269354b4f8b4c1f6616ab68e710379bc3
Hakeem Iqbal <hakeemiqbalnew@gmail.com>
2025-05-13 20:14:24 +0530

    Add IOException handling to registerEffect methods in EffectManager interface

--- src/main/java/com/toonystank/effect/EffectManager.java
+++ src/main/java/com/toonystank/effect/EffectManager.java
@@ -1,17 +1,18 @@
 package com.toonystank.effect;
 
+import java.io.IOException;
 import java.util.List;
 
 public interface EffectManager {
 
 
-    BaseEffect.Effect registerEffect(BaseEffect.Effect effect);
+    BaseEffect.Effect registerEffect(BaseEffect.Effect effect) throws IOException;
     BaseEffect.Effect registerEffect(String name,
                                      boolean enabled,
                                      List<String> description,
                                      List<String> commandsToRun,
                                      String permissionNode,
-                                     Class<? extends BaseEffect> effectClass);
+                                     Class<? extends BaseEffect> effectClass) throws IOException;
 
 
     BaseEffect.Effect getEffect(String name);
